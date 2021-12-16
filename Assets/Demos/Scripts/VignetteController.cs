d7051928c94de2f376e2a6659a6e03a694bde516
hiroaki-1017 <yh111017@outlook.jp>
2021-12-16 17:21:05 +0900

    [update]vignette

--- Assets/Demos/Scripts/VignetteController.cs
+++ Assets/Demos/Scripts/VignetteController.cs
@@ -5,7 +5,7 @@ using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.Rendering.PostProcessing;
 
-public class Vignette : MonoBehaviourPunCallbacks
+public class VignetteController : MonoBehaviourPunCallbacks
 {
     private Rigidbody rb;
     private Vector3 cleanVignette;
@@ -19,12 +19,12 @@ public class Vignette : MonoBehaviourPunCallbacks
     {
         rb = this.GetComponent<Rigidbody>();
         cleanVignette = GameObject.Find("vignette").transform.position;
-        postProcessGameObject = GameObject.Find("vignetteVolume");
-        // m_Vignette = ScriptableObject.CreateInstance<Vignette>();
-        // m_Vignette.enabled.Override(true);
-        // m_Vignette.center.Override((1, 0.5));
-        // m_Volume = PostProcessManager.instance.QuickVolume(postProcessGameObject.layer, 100f, m_Vignette);
-        // Debug.Log(m_Vignette.center.value);
+        postProcessGameObject = GameObject.Find("PostProcessingGO");
+        m_Vignette = ScriptableObject.CreateInstance<Vignette>();
+        m_Vignette.enabled.Override(true);
+        m_Vignette.center.Override(new Vector2(0.5f, 0.5f));
+        m_Volume = PostProcessManager.instance.QuickVolume(postProcessGameObject.layer, 100f, m_Vignette);
+        Debug.Log(m_Vignette.center.value);
 
     }
 
@@ -38,6 +38,7 @@ public class Vignette : MonoBehaviourPunCallbacks
                 me = this.gameObject.transform.position;
                 disz = cleanVignette.z - me.z;
                 Debug.Log(disz);
+                m_Vignette.center.value = new Vector2(0.5f - Mathf.Clamp(disz, -2f, 3f), 0.5f);
             }
         }
 
